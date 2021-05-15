27f6f0d9cc2c1dbc08ce495664ae856dcc4e54c9
myasinkaji <myasinkaji@gmail.com>
2021-05-15 21:37:42 +0430

    update page

--- src/page/centralguild/CentralGuildForm.js
+++ src/page/centralguild/CentralGuildForm.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Button, Grid, makeStyles, Paper} from "@material-ui/core";
 import * as Service from '../../service/CentralGuildService';
 import PageHeader from "../../component/PageHeader";
+import PeopleOutlineIcon from "@material-ui/icons/PeopleOutline";
 
 const useStyles = makeStyles(theme => ({
     paper: {
@@ -18,14 +19,19 @@ const CentralGuildForm = () => {
     return (
         <>
             <Grid item xs={12}>
-                <PageHeader/>
+                <PageHeader
+                    icon={<PeopleOutlineIcon/>}
+                    title='Central-Guild Form'
+                    subtitle='central-guild subtitle is here'/>
             </Grid>
             <Grid item xs={12}>
-                <Paper className={classes.paper}>This is Central-Guild page</Paper>
+                <Paper square className={classes.paper}>This is Central-Guild page</Paper>
                 <Button
                     variant='contained'
                     color='primary'
-                    onClick={() => {Service.getPage(1, 10)}}>Get Page...</Button>
+                    onClick={() => {
+                        Service.getPage(1, 10)
+                    }}>Get Page...</Button>
             </Grid>
         </>
     );
