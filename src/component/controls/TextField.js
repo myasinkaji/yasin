52752d4aa8cfaf666d28ecbcb5c321a292a76d29
myasinkaji <myasinkaji@gmail.com>
2021-05-19 00:05:35 +0430

    add error property to TextField

--- src/component/controls/TextField.js
+++ src/component/controls/TextField.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {TextField as MuiTextField} from "@material-ui/core";
 
 const TextField = (props) => {
+    const {error} = props;
 
     return (
         <MuiTextField
             size='small'
             {...props}
+            {...(error ? {error: true, helperText: error} : {})}
             style={{width: '90%'}}/>
     );
 }
