29ac0d1e96234ed784b73b05d3091b18481fa692
myasinkaji <myasinkaji@gmail.com>
2021-05-24 09:22:49 +0430

    add constants

--- src/service/Constants.js
+++ src/service/Constants.js
@@ -3,3 +3,6 @@ export const DEFAULT_ORDER = 'asc';
 
 export const EMPTY_PAGE = {data: [], count: 0};
 export const NO_ERROR = {};
+
+export const DEFAULT_ROWS_PER_PAGE_OPTIONS = [5, 10, 20];
+export const DEFAULT_ROWS_PER_PAGE = DEFAULT_ROWS_PER_PAGE_OPTIONS[0];
