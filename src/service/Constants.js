6912108b1ac1f08082349de660ce50a3c964ccb6
myasinkaji <myasinkaji@gmail.com>
2021-05-28 01:38:59 +0430

    change default table size to 10

--- src/service/Constants.js
+++ src/service/Constants.js
@@ -4,4 +4,4 @@ export const EMPTY_PAGE = {data: [], count: 0};
 export const NO_ERROR = {};
 
 export const DEFAULT_ROWS_PER_PAGE_OPTIONS = [5, 10, 20];
-export const DEFAULT_ROWS_PER_PAGE = DEFAULT_ROWS_PER_PAGE_OPTIONS[0];
+export const DEFAULT_ROWS_PER_PAGE = DEFAULT_ROWS_PER_PAGE_OPTIONS[1];
