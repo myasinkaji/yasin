e946b66e8b42fbefd5ff9924114fde70dd7a2572
myasinkaji <myasinkaji@gmail.com>
2021-05-29 23:07:10 +0430

    initial copy

---
+++ src/page/contractor/ProvinceGuildForm.js
@@ -0,0 +1,142 @@
+import React, {useEffect, useState} from 'react';
+import {DialogActions, DialogContent, Grid, makeStyles} from "@material-ui/core";
+import TextField from "../../component/controls/TextField";
+import Button from "../../component/controls/Button";
+import * as Service from '../../service/provinceGuild/ProvinceGuildService';
+import * as CountryDivisionService from '../../service/countrydivision/CountryDivisionService';
+import * as CentralGuildService from '../../service/centralGuild/CentralGuildService';
+import * as BaseService from '../../service/BaseService';
+import * as Constants from '../../service/Constants';
+import Checkbox from "../../component/controls/Checkbox";
+import AutoComplete from "../../component/controls/AutoComplete";
+
+
+const useStyle = makeStyles(theme => ({
+    dialogContent: {
+        backgroundColor: 'pink',
+        paddingBottom: theme.spacing(4)
+    }
+}));
+
+
+const ProvinceGuildForm = (props) => {
+    const classes = useStyle();
+    const {recordForUpdate, submitAware, setNotify} = props;
+    const initialValue = recordForUpdate ? recordForUpdate : Service.INITIAL_GUILD;
+    const [guild, setGuild] = useState(initialValue);
+    const [errors, setErrors] = useState(Constants.NO_ERROR);
+    const [countryDivisions, setCountryDivisions] = useState([]);
+    const [centralGuilds, setCentralGuilds] = useState([]);
+
+    useEffect(() => {
+        CountryDivisionService.getLazy()
+            .then(response => setCountryDivisions(response.data.data))
+            .catch(e => setNotify(BaseService.getErrorMessageObject(`Error Code: ${e.status}, Message: ${e.name}`)));
+
+        CentralGuildService.getLazy()
+            .then(response => setCentralGuilds(response.data.data))
+            .catch(e => setNotify(BaseService.getErrorMessageObject(`Error Code: ${e.status}, Message: ${e.name}`)));
+    }, []);
+
+    function onchange(event) {
+        const {name, value} = event.target;
+        setGuild({
+            ...guild,
+            [name]: value,
+        });
+    }
+
+    function register() {
+        if (Service.validate(guild, setErrors)) {
+            Service.save(guild).then(() => {
+                submitAware(guild);
+            }).catch(e => {
+                setNotify(BaseService.getErrorMessageObject(`Error Code: ${e.status}, Message: ${e.name}`));
+            })
+        }
+    }
+
+    function reset() {
+        setGuild(initialValue);
+    }
+
+    return (
+        <>
+            <DialogContent className={classes.dialogContent}>
+                <Grid container spacing={3}>
+                    <Grid item container xs={12} md={6} spacing={3}>
+                        <Grid item xs={12}>
+                            <TextField error={errors.code}
+                                       onChange={onchange} name='code' label='Code' value={guild.code}/>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField error={errors.name}
+                                       onChange={onchange} name='name' label='Name' value={guild.name}/>
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <TextField error={errors.uniqueId}
+                                       onChange={onchange} name='uniqueId' label='UniqueId' value={guild.uniqueId}/>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField error={errors.postalCode}
+                                       onChange={onchange} name='postalCode' label='Postal Code'
+                                       value={guild.postalCode}/>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField error={errors.managerName}
+                                       onChange={onchange} name='managerName' label='Manager Name'
+                                       value={guild.managerName}/>
+                        </Grid>
+                    </Grid>
+                    <Grid item container xs={12} md={6} spacing={3}>
+
+                        <Grid item xs={12}>
+                            <TextField error={errors.phone}
+                                       onChange={onchange} name='phone' label='Phone' value={guild.phone}/>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField error={errors.mobile}
+                                       onChange={onchange} name='mobile' label='Mobile' value={guild.mobile}/>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <AutoComplete error={errors.centralGuildCode}
+                                          data={centralGuilds}
+                                          onChange={onchange} name='centralGuildCode'
+                                          label='Central Guild'/>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <AutoComplete error={errors.countryDivisionId}
+                                          data={countryDivisions}
+                                          onChange={onchange} name='countryDivisionId'
+                                          label='Country Division'/>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Checkbox
+                                name='active'
+                                label="Is Active"
+                                checked={guild.active}
+                                onChange={onchange}
+                            />
+                        </Grid>
+                    </Grid>
+
+
+                </Grid>
+            </DialogContent>
+
+            <DialogActions>
+                <Grid container>
+                    <Grid item xs={12} sm={6}>
+                        <Button label='Reset' color='secondary' onClick={reset} type='reset'/>
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <Button label='Save Entity' onClick={register} type='submit'/>
+                    </Grid>
+                </Grid>
+            </DialogActions>
+        </>
+    );
+}
+
+export default ProvinceGuildForm;
---
+++ src/page/contractor/ProvinceGuildTable.js
@@ -0,0 +1,144 @@
+import React, {useEffect, useState} from 'react';
+import {
+    Checkbox,
+    IconButton,
+    makeStyles,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead, TablePagination,
+    TableRow, TableSortLabel
+} from "@material-ui/core";
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import * as Constants from '../../service/Constants';
+import * as Service from '../../service/provinceGuild/ProvinceGuildService';
+
+const useStyles = makeStyles(theme => ({
+    checkbox: {
+        padding: theme.spacing(0),
+    },
+    cell: {
+        paddingLeft: theme.spacing(1),
+        paddingRight: theme.spacing(1),
+        padding: theme.spacing(0),
+    },
+    head: {
+        backgroundColor: theme.palette.primary.dark
+    },
+    evenRow: {
+        backgroundColor: theme.palette.secondary.main
+    },
+    oddRow: {
+        backgroundColor: theme.palette.secondary.light
+    },
+}));
+
+const HEADERS = [
+    {id: 'uniqueId', title: 'Unique Id', sortable: true},
+    {id: 'code', title: 'Code', sortable: true},
+    {id: 'managerName', title: 'Manager Name', sortable: true},
+    {id: 'name', title: 'Name', sortable: true},
+    {id: 'postalCode', title: 'Postal Code', sortable: false},
+    {id: 'centralGuild', title: 'Central Guild', sortable: true},
+    {id: 'countryDivision', title: 'Country Division', sortable: true},
+    {id: 'active', title: 'Active', sortable: true},
+    {id: 'phone', title: 'Phone', sortable: false},
+    {id: 'mobile', title: 'Mobile', sortable: false},
+];
+
+const ProvinceGuildTable = (props) => {
+    const classes = useStyles();
+    const {pageData, onEditClick, onDeleteClick, loadPage} = props;
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(Constants.DEFAULT_ROWS_PER_PAGE);
+    const [orderBy, setOrderBy] = useState(Service.DEFAULT_PAGE_REQUEST.orderBy);
+    const [order, setOrder] = useState(Constants.DEFAULT_ORDER);
+
+    useEffect(() => {
+        loadPage({
+            page,
+            pageSize: rowsPerPage,
+            order,
+            orderBy
+        })
+    }, [page, rowsPerPage, orderBy, order]);
+    function changePage(event, newPage) {
+        setPage(newPage);
+    }
+
+    function changeRowsPerPage(event) {
+        setRowsPerPage(event.target.value);
+    }
+
+    function orderChanged(newOrderBy) {
+        setOrder(orderBy === newOrderBy ? (order === 'asc' ? 'desc' : 'asc') : 'asc')
+        setOrderBy(newOrderBy);
+    }
+    return (
+        <TableContainer component={Paper}>
+            <Table size="small">
+                <TableHead className={classes.head}>
+                    <TableRow>
+                        <TableCell align='center'>Id</TableCell>
+                        {
+                            HEADERS.map(header => (<TableCell align='center' key={header.id}>
+                                    {header.sortable ?
+                                        <TableSortLabel
+                                            active={orderBy === header.id}
+                                            direction={orderBy === header.id ? order : 'asc'}
+                                            onClick={() => orderChanged(header.id)}>
+                                            {header.title}
+                                        </TableSortLabel>
+                                        : header.title
+                                    }
+                            </TableCell>))
+                        }
+                        <TableCell align='center'>Actions</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {pageData.data.map((guild, index) => (
+                        <TableRow className={index % 2 === 0 ? classes.evenRow : ''} key={guild.code}>
+                            <TableCell className={classes.cell} align='center'>{(page * rowsPerPage) + index+1}</TableCell>
+                            <TableCell className={classes.cell} align='center'>{guild.uniqueId}</TableCell>
+                            <TableCell className={classes.cell} align='center'>{guild.code}</TableCell>
+                            <TableCell className={classes.cell} align='center'>{guild.managerName}</TableCell>
+                            <TableCell className={classes.cell} align='center'>{guild.name}</TableCell>
+                            <TableCell className={classes.cell} align='center'>{guild.postalCode}</TableCell>
+                            <TableCell className={classes.cell} align='center'>{guild.centralGuildName}</TableCell>
+                            <TableCell className={classes.cell} align='center'>{guild.countryDivisionName}</TableCell>
+                            <TableCell className={classes.cell} align='center'>
+                                <Checkbox className={classes.checkbox} color='primary'
+                                          size='small' checked={guild.active} disableRipple/>
+                            </TableCell>
+                            <TableCell className={classes.cell} align='center'>{guild.phone}</TableCell>
+                            <TableCell className={classes.cell} align='center'>{guild.mobile}</TableCell>
+                            <TableCell className={classes.cell} align='center'>
+                                <IconButton size='small' onClick={() => onDeleteClick(guild)}>
+                                    <DeleteIcon fontSize='small' color="primary"/>
+                                </IconButton>
+                                <IconButton size='small' onClick={() => onEditClick(guild)}>
+                                    <EditIcon fontSize='small' color="primary"/>
+                                </IconButton>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+            <TablePagination
+                component="div"
+                rowsPerPageOptions={Constants.DEFAULT_ROWS_PER_PAGE_OPTIONS}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                count={pageData.count}
+                onChangePage={changePage}
+                onChangeRowsPerPage={changeRowsPerPage}
+            />
+        </TableContainer>
+    );
+}
+
+export default ProvinceGuildTable;
