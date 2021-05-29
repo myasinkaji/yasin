a2f8df720656d62176e444dbf69c76eb8c7ab9ed
myasinkaji <myasinkaji@gmail.com>
2021-05-29 23:38:51 +0430

    proprietary code

--- src/service/contractor/ContractorService.js
+++ src/service/contractor/ContractorService.js
@@ -2,31 +2,35 @@ import RestService from "../rest/RestService";
 import * as BaseService from '../../service/BaseService';
 import * as Constants from "../Constants";
 
-const BASE_ADDRESS = '/pg';
+const BASE_ADDRESS = '/contractor';
 const SEARCH_ADDRESS = BASE_ADDRESS.concat('/search')
 
-export const GUILD_SEARCH_CRITERIA = {
-    uniqueId: '',
-    code: '',
-    name: '',
-    countryDivisionId: '',
-    centralGuildCode: '',
-    active: false,
+export const CONTRACTOR_SEARCH_CRITERIA = {
+    firstname: '',
+    lastname: '',
+    nationalCode: '',
+    birthDate: '',
+    postalCode: '',
     phone: '',
-    mobile: ''
+    email: '',
+    uniqueId: '',
+    companyName: '',
+    countryDivisionId: '',
+    provinceGuildId: '',
 };
 
-export const INITIAL_GUILD = {
-    uniqueId: '',
-    code: '',
-    countryDivisionId: '',
-    centralGuildCode: '',
-    managerName: '',
-    name: '',
+export const INITIAL_CONTRACTOR = {
+    firstname: '',
+    lastname: '',
+    nationalCode: '',
+    birthDate: '',
     postalCode: '',
-    active: false,
     phone: '',
-    mobile: ''
+    email: '',
+    uniqueId: '',
+    companyName: '',
+    countryDivisionId: '',
+    provinceGuildId: '',
 }
 export const DEFAULT_PAGE_REQUEST = {
     page: 0,
@@ -42,32 +46,34 @@ export function getPage(pageRequest) {
     return RestService.get(url)
 }
 
-export function remove(guildCode) {
+export function remove(contractorCode) {
     const url = BASE_ADDRESS
         .concat('?')
         .concat('code=')
-        .concat(guildCode);
+        .concat(contractorCode);
     return RestService.delete(url);
 }
-export function save(guild) {
-    return RestService.post(BASE_ADDRESS, guild);
+export function save(contractor) {
+    return RestService.post(BASE_ADDRESS, contractor);
 }
 
 export function search(pageRequest, searchCriteria) {
     return BaseService.search(SEARCH_ADDRESS, pageRequest, searchCriteria);
 }
 
-export function validate(guild, setErrors) {
+export function validate(contractor, setErrors) {
     const errors = {};
-    errors.code = isBlank(String(guild.code)) ? 'code is required' : '';
-    errors.name = isBlank(guild.name) ? 'name is required' : '';
-    errors.uniqueId = isBlank(String(guild.uniqueId)) ? 'unique id is required' : '';
-    errors.countryDivisionId = isBlank(String(guild.countryDivisionId)) ? 'country division is required' : '';
-    errors.centralGuildCode = isBlank(String(guild.centralGuildCode)) ? 'central guild is required' : '';
-    errors.postalCode = isBlank(String(guild.postalCode)) ? 'postal code is required' : '';
-    errors.managerName = isBlank(guild.managerName) ? 'manager name is required' : '';
-    errors.phone = isBlank(guild.phone) ? 'phone is required' : '';
-    errors.mobile = isBlank(guild.mobile) ? 'mobile is required' : '';
+    errors.firstname = isBlank(contractor.firstname) ? 'firstname is required' : '';
+    errors.lastname = isBlank(contractor.lastname) ? 'firstname is required' : '';
+    errors.nationalCode = isBlank(String(contractor.nationalCode)) ? 'national code is required' : '';
+    errors.birthDate = isBlank(String(contractor.birthDate)) ? 'birth date is required' : '';
+    errors.postalCode = isBlank(String(contractor.postalCode)) ? 'postal code is required' : '';
+    errors.phone = isBlank(contractor.phone) ? 'phone is required' : '';
+    errors.email = isBlank(contractor.email) ? 'email is required' : '';
+    errors.uniqueId = isBlank(String(contractor.uniqueId)) ? 'unique id is required' : '';
+    errors.companyName = isBlank(contractor.companyName) ? 'companyName is required' : '';
+    errors.countryDivisionId = isBlank(String(contractor.countryDivisionId)) ? 'country division is required' : '';
+    errors.provinceGuildId = isBlank(String(contractor.provinceGuildId)) ? 'province guild is required' : '';
 
     setErrors({...errors})
     return Object.values(errors).every(isBlank);
